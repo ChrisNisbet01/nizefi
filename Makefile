50ca94a0379daa9caca25327cfd118364696a2ba
Chris Nisbet <nischris@gmail.com>
2017-03-02 15:47:25 +1300

    Building on Linux. Can program from Linux using make program as well.

---
+++ Makefile
@@ -0,0 +1,200 @@
+CYGWIN=nodosfilewarning
+
+# setup executables
+TOOLCHAIN_PATH ?= /usr/local/gcc-arm-none-eabi-5_4-2016q3/bin
+TOOLCHAIN_PREFIX ?= arm-none-eabi-
+CC_PREFIX ?= $(TOOLCHAIN_PATH)/$(TOOLCHAIN_PREFIX)
+CC = $(CC_PREFIX)gcc
+OBJCOPY		 = $(CC_PREFIX)objcopy
+OBJDUMP		 = $(CC_PREFIX)objdump
+SIZE		 = $(CC_PREFIX)size
+CO_FLASH     = /cygdrive/c/CooCox/CoIDE/bin/coflash.exe
+
+# location of OpenOCD Board .cfg files (only used with 'make program')
+OPENOCD_BOARD_DIR=/usr/share/openocd/scripts/board
+
+# Configuration (cfg) file containing programming directives for OpenOCD
+OPENOCD_PROC_FILE=extra/stm32f4-openocd.cfg
+
+# indicate which platform we're building for
+TARGET ?= STM32F4_DISC1
+TARGETS = STM32F4_DISC1
+
+# check for valid target
+ifeq ($(TARGET),$(filter $(TARGET),$(TARGETS)),)
+$(error Invalid Target '$(TARGET)'. Valid targets are $(TARGETS))
+endif
+
+USER_OPTIONS ?=
+
+# directories
+ROOT         := .
+SRC_DIR		 = $(ROOT)
+OBJ_DIR	     = $(ROOT)/obj
+BIN_DIR		 = $(ROOT)/bin
+COOS_DIR     = $(SRC_DIR)/CoOS
+TARGET_HEX   = $(BIN_DIR)/$(TARGET).hex
+TARGET_ELF   = $(BIN_DIR)/$(TARGET).elf
+TARGET_DIS   = $(BIN_DIR)/$(TARGET).dis
+TARGET_MAP   = $(BIN_DIR)/$(TARGET).map
+
+COMMON_CFLAGS = -ffunction-sections \
+                -fdata-sections \
+				-g \
+				-Wall \
+				-Wextra
+
+OPTIMISE_FLAGS = -O3
+
+
+INCLUDE_DIRS = 	$(SRC_DIR)/stdio \
+				$(SRC_DIR)/UART \
+				$(COOS_DIR) \
+				$(COOS_DIR)/portable \
+				$(COOS_DIR)/kernel
+
+ifeq ($(TARGET),STM32F4_DISC1)
+
+INCLUDE_DIRS := $(INCLUDE_DIRS) \
+				$(SRC_DIR)/cmsis_boot/startup \
+				$(SRC_DIR)/cmsis_boot \
+				$(SRC_DIR)/cmsis_lib/include \
+				$(SRC_DIR)/cmsis
+
+
+CPU_FLAGS = -mcpu=cortex-m4 -mthumb
+CPU_DEFINES = -DSTM32F4XX
+
+# use hardware floating point
+FPU_FLAGS = -mfpu=fpv4-sp-d16 -mfloat-abi=hard
+FPU_DEFINES = -D__FPU_USED
+
+LINK_SCRIPT = $(ROOT)/link.ld
+CO_FLASH_PROCESSOR_TYPE = STM32F407VG
+
+CMSIS_BOOT_SRC = $(SRC_DIR)/cmsis_boot/startup/startup_stm32f4xx.c \
+                 $(SRC_DIR)/cmsis_boot/*.c
+
+CMSIS_LIB_SRC = $(SRC_DIR)/cmsis_lib/source/*.c
+
+TARGET_SRC = $(CMSIS_BOOT_SRC) \
+             $(CMSIS_LIB_SRC)
+
+endif
+
+#LTO_FLAGS	 = -flto -fuse-linker-plugin
+
+CFLAGS = $(COMMON_CFLAGS) \
+			$(OPTIMISE_FLAGS) \
+			-D'__TARGET__="$(TARGET)"' \
+			-DUSE_STDPERIPH_DRIVER \
+			$(addprefix -I,$(INCLUDE_DIRS)) \
+			$(CPU_FLAGS) \
+			$(CPU_DEFINES) \
+			$(FPU_FLAGS) \
+			$(FPU_DEFINES) \
+			$(addprefix -D,$(PLATFORM_FLAGS)) \
+			$(addprefix -D,$(USER_OPTIONS)) \
+			-MMD
+
+COMMON_LDFLAGS = -g \
+                 -Wall \
+                 $(OPTIMISE_FLAGS) \
+                 -Wl,--gc-sections \
+                 -nostartfiles \
+                 -lm
+		         --specs=nano.specs \
+		         -lc \
+		         -lnosys
+
+LDFLAGS = $(CPU_FLAGS) \
+          $(FPU_FLAGS) \
+          $(COMMON_LDFLAGS) \
+          -Wl,-Map=$(TARGET_MAP) \
+          $(LTO_FLAGS) \
+          -T$(LINK_SCRIPT)
+
+# now specify source files
+COMMON_SRC = $(SRC_DIR)/stdio/*.c \
+			 $(SRC_DIR)/main.c \
+			 $(SRC_DIR)/UART/*.c
+
+COOS_SRC = $(COOS_DIR)/kernel/*.c \
+           $(COOS_DIR)/portable/*.c
+
+COOS_SRC_NO_LTO = $(wildcard $(COOS_DIR)/portable/GCC/*.c)
+
+
+SRC_FILES = $(COMMON_SRC) \
+            $(TARGET_SRC) \
+            $(STM32_SRC) \
+            $(COOS_SRC)
+
+SRC_FILES_NO_LTO = $(COOS_SRC_NO_LTO) \
+				$(SRC_DIR)/syscalls/*.c
+
+
+# add .c files to object list
+OBJS = $(patsubst %.c,%.o,$(wildcard $(SRC_FILES)))
+# add .S files to object list
+OBJS := $(patsubst %.S,%.o,$(OBJS))
+# prepend obj directory to object list
+OBJS := $(OBJS:%=$(OBJ_DIR)/%)
+
+OBJS_NO_LTO = $(patsubst %.c,%.o,$(wildcard $(SRC_FILES_NO_LTO)))
+# prepend obj directory to object list
+OBJS_NO_LTO := $(OBJS_NO_LTO:%=$(OBJ_DIR)/no_lto/%)
+
+
+TARGET_DEPENDENCIES = $(patsubst %.o,%.d,$(OBJS)) $(patsubst %.o,%.d,$(OBJS_NO_LTO))
+
+
+all: $(TARGET_HEX)
+
+program: $(TARGET_ELF)
+	openocd -f $(OPENOCD_PROC_FILE) -c "stm32f4_flash $(TARGET_HEX)" -c shutdown
+
+flash: all
+	$(CO_FLASH) program $(CO_FLASH_PROCESSOR_TYPE) $(TARGET_ELF) --adapter-name=ST-Link
+
+dump: all
+	$(OBJDUMP) -D $(TARGET_ELF) > $(TARGET_DIS)
+
+$(TARGET_HEX): $(TARGET_ELF)
+	$(OBJCOPY) -O ihex --set-start 0x8000000 $< $@
+
+$(TARGET_ELF): $(OBJS) $(OBJS_NO_LTO)
+	echo "OBJS: $(OBJS) $(OBJS_NO_LTO)"
+	$(CC) -o $@ $^ $(LDFLAGS)
+	$(SIZE) $(TARGET_ELF)
+
+$(OBJ_DIR)/no_lto/%.o: CFLAGS_NO_LTO=$(filter-out -flto, $(CFLAGS))
+$(OBJ_DIR)/no_lto/%.o : %.c
+	mkdir -p $(dir $@)
+	$(CC) -c -o $@ $< $(CFLAGS_NO_LTO)
+
+$(OBJ_DIR)/%.o : %.c
+	mkdir -p $(dir $@)
+	$(CC) -c -o $@ $< $(CFLAGS)
+
+$(OBJ_DIR)/%.o: %.s
+	echo $<
+	mkdir -p $(dir $@)
+	$(CC) -c -o $@ $< $(CFLAGS)
+
+$(OBJ_DIR)/%.o: %.S
+	echo $<
+	mkdir -p $(dir $@)
+	$(CC) -c -o $@ $< $(CFLAGS)
+
+
+.PHONY:	clean
+
+
+clean:
+	rm -rf $(OBJS)
+	rm -rf $(TARGET_ELF)
+	rm -rf $(TARGET_HEX)
+
+-include $(TARGET_DEPENDENCIES)
+
---
+++ main.c
@@ -0,0 +1,203 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  psavr@gmx.ch
+  * @version V1.0
+  * @date    17-Oktober-2012
+  * @brief   Demo-Application for STM32F4-Discovery Board
+  *          including RTOS CoOS V1.1.4 (2011.04.20)
+  *          including printf() for USART2 (=Default) or USART3
+  ******************************************************************************
+  * @attention
+  *
+  * This Example is maintained under the PsAvr "BEERWARE-LICENSE"
+  * Feel free to use it, to modify it and to distribute it. If you like
+  * it and we meet us once in a pub or a bar, feel free to spend me a beer!
+  *
+  * I would appreciate it very much, if you would return any corrections
+  * and/or improvements back to me and/or to the CooCox community.
+  ******************************************************************************
+  */
+
+/*---------------------------- Include ---------------------------------------*/
+#include <stdio.h>
+#include <CoOS.h>
+#include "stm32f4xx.h"
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_rcc.h"
+#include "usart.h"
+
+/*---------------------------- Symbol Define -------------------------------*/
+#define STACK_SIZE_TASKA 128              /*!< Define "taskA" task size */
+#define STACK_SIZE_TASKB 128              /*!< Define "taskB" task size */
+#define STACK_SIZE_TASKC 128              /*!< Define "taskC" task size */
+#define STACK_SIZE_TASKD 128              /*!< Define "taskD" task size */
+
+/* Private typedef -----------------------------------------------------------*/
+GPIO_InitTypeDef GPIO_InitStructure;
+/*---------------------------- Variable Define -------------------------------*/
+OS_STK taskA_stk[STACK_SIZE_TASKA]; /*!< Define "taskA" task stack */
+OS_STK taskB_stk[STACK_SIZE_TASKB]; /*!< Define "taskB" task stack */
+OS_STK taskC_stk[STACK_SIZE_TASKC]; /*!< Define "taskC" task stack */
+OS_STK taskD_stk[STACK_SIZE_TASKD]; /*!< Define "taskD" task stack */
+
+/**
+ *******************************************************************************
+ * @brief       "taskA" task code
+ * @param[in]   None
+ * @param[out]  None
+ * @retval      None
+ * @par Description
+ * @details    This task use to crate mutex and flags,print message "taskA running".
+ *             Indicate "taskA" had been executed.
+ *******************************************************************************
+ */
+void taskA(void* pdata)
+{
+  unsigned int a = 0;
+  printf("CoOS taskA: started\n");
+  while (1)
+    {
+      if (a & 1)
+        {
+          GPIO_SetBits(GPIOD, GPIO_Pin_12);
+        }
+      else
+        {
+          GPIO_ResetBits(GPIOD, GPIO_Pin_12);
+        }
+      a++;
+      CoTickDelay(25);  //25 x 10ms = 250ms
+    }
+}
+
+/**
+ *******************************************************************************
+ * @brief       "taskB" task code
+ * @param[in]   None
+ * @param[out]  None
+ * @retval      None
+ * @par Description
+ * @details    This task use to print message "taskB running". Indicate "taskB"
+ *             had been executed.
+ *******************************************************************************
+ */
+void taskB(void* pdata)
+{
+  unsigned int b = 0;
+  printf("CoOS taskB: started\n");
+  while (1)
+    {
+      if (b & 1)
+        {
+          GPIO_SetBits(GPIOD, GPIO_Pin_13);
+        }
+      else
+        {
+          GPIO_ResetBits(GPIOD, GPIO_Pin_13);
+        }
+      b++;
+      CoTickDelay(25);  //50 x 10ms = 500ms
+    }
+}
+
+/**
+ *******************************************************************************
+ * @brief       "taskC" task code
+ * @param[in]   None
+ * @param[out]  None
+ * @retval      None
+ * @par Description
+ * @details    This task use to print message "taskB running". Indicate "taskB"
+ *             had been executed.
+ *******************************************************************************
+ */
+void taskC(void* pdata)
+{
+  unsigned int c = 0;
+  printf("CoOS taskC: started\n");
+  while (1)
+    {
+      if (c & 1)
+        {
+          GPIO_SetBits(GPIOD, GPIO_Pin_14);
+        }
+      else
+        {
+          GPIO_ResetBits(GPIOD, GPIO_Pin_14);
+        }
+      c++;
+      CoTickDelay(25);  //100 x 10ms = 1000ms
+    }
+}
+
+
+/**
+ *******************************************************************************
+ * @brief       "taskC" task code
+ * @param[in]   None
+ * @param[out]  None
+ * @retval      None
+ * @par Description
+ * @details    This task use to print message "taskB running". Indicate "taskB"
+ *             had been executed.
+ *******************************************************************************
+ */
+void taskD(void* pdata)
+{
+  unsigned int d = 0;
+  printf("CoOS taskD: started\n");
+  while (1)
+    {
+      if (d & 1)
+        {
+          GPIO_ResetBits(GPIOD, GPIO_Pin_15);
+        }
+      else
+        {
+          GPIO_SetBits(GPIOD, GPIO_Pin_15);
+        }
+      d++;
+      CoTickDelay(50);  //50 x 10ms = 500ms
+    }
+}
+
+
+int main(void)
+{
+  SystemInit();
+  USART_Configuration();
+  printf("STM32F4-Discovery Board is booting!\n");
+  printf("-----------------------------------\n");
+  printf("CoOS RTOS V-1.1.4; Demo for STM32F4\n");
+  printf("V-01 17.10.2012 (w) by psavr@gmx.ch\n");
+  printf("-----------------------------------\n");
+
+  /* GPIOD Periph clock enable */
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+  /* Configure PD12, PD13, PD14 and PD15 in output pushpull mode */
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14  | GPIO_Pin_15;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  GPIO_Init(GPIOD, &GPIO_InitStructure);
+
+  CoInitOS(); /*!< Initial CooCox CoOS          */
+
+  /*!< Create three tasks	*/
+  printf("CoOS RTOS: Creating tasks\n");
+  CoCreateTask(taskA, 0, 0, &taskA_stk[STACK_SIZE_TASKA-1], STACK_SIZE_TASKA);
+  CoCreateTask(taskB, 0, 1, &taskB_stk[STACK_SIZE_TASKB-1], STACK_SIZE_TASKB);
+  CoCreateTask(taskC, 0, 2, &taskC_stk[STACK_SIZE_TASKC-1], STACK_SIZE_TASKC);
+  CoCreateTask(taskD, 0, 3, &taskD_stk[STACK_SIZE_TASKD-1], STACK_SIZE_TASKD);
+
+  printf("CoOS RTOS: Starting scheduler\n");
+  CoStartOS(); /*!< Start multitask	           */
+
+  printf("CoOS RTOS: Scheduler stopped\n");
+  while (1)
+  {
+  }
+}
+
